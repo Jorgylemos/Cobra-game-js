a4e2e9af2ac00b87db4c5cdda46c731da4a5499e
jorgylemos <jorgylemos@gmail.com>
2021-06-01 11:39:27 -0300

    controle update

--- js/main.js
+++ js/main.js
@@ -30,14 +30,34 @@ function cobrinha(){
     }
 }
 
+document.addEventListener("keydown", update); //Aqui ele está pegando o evento de click (Ele está esperando a ação) e junto está chamando o update.
+function update(event){
+    
+    //Ela só muda se a posição não for contrária a ela.
+    if(event.keyCode == 37 && direcao != "direita") direcao = "esquerda"; 
+    if(event.keyCode == 38 && direcao != "baixo") direcao = "cima";
+
+    if(event.keyCode == 39 && direcao != "esquerda") direcao = "direta";
+    if(event.keyCode == 40 && direcao != "cima") direcao = "baixo";
+    //Ao apertar alguma tecla, o event vai chamar a update e logo após irá passar como argumento o evento de tecla setada.
+
+}
 
 function iniciarjogo(){ //Função que irá iniciar e definir o controle do jogo
+
+    //Plano cartesiano para fazer ele retornar após sair da tela.
+    if(cobra[0].x > 15 * pixel && direcao == "direta") cobra[0].x = 0;
+    if(cobra[0].x < 0 && direcao == "esquerda") cobra[0].x = 16 * pixel;
+    
+    if(cobra[0].y > 15 * pixel && direcao == "baixo") cobra[0].y = 0;
+    if(cobra[0].y < 0 && direcao == "cima") cobra[0].y = 16 * pixel;
+
     background();
     cobrinha();
 
     let cobra_X = cobra[0].x;
     let cobra_Y = cobra[0].y;
-
+    
     if(direcao == "direta") cobra_X += pixel;
     if(direcao == "esquerda") cobra_X -= pixel;
 
